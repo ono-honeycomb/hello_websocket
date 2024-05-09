551dab9d742bb01d44221d5a0b44fcd46c7f42e1
ono <ono@honeycomb-lab.co.jp>
2024-05-09 10:59:36 +0900

    test

--- app.js
+++ app.js
@@ -9,38 +9,44 @@ const io = new Server(httpServer);
 
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/', (req, res) => {
-    io.emit('message', JSON.stringify(req.body));
-    console.log(req.body.subject + req.body.text);
-    res.sendStatus(200);
+// app.post('/', (req, res) => {
+// 	io.emit('message', JSON.stringify(req.body));
+// 	console.log(req.body.subject + req.body.text);
+// 	res.sendStatus(200);
+// });
+
+// app.post('/parse/', (req, res) => {
+// 	io.emit('message', req.body.text);
+// 	console.log(req.body);
+// 	res.sendStatus(200);
+// });
+
+// app.post('/parse/400', (req, res) => {
+// 	io.emit('message', '400' + req.body);
+// 	console.log('400');
+// 	res.sendStatus(400);
+// });
+
+// app.post('/parse/500', (req, res) => {
+// 	io.emit('message', '500');
+// 	console.log('500');
+// 	res.sendStatus(500);
+// });
+
+io.on('connection', (socket) => {
+	console.log('connection id:' + socket.id);
+	socket.on('disconnect', () => {
+		console.log('disconnect id:' + socket.id);
+	});
+
+	socket.on('message', (message) => {
+		io.emit('message', message);
+	});
 });
 
-app.post('/parse/', (req, res) => {
-    io.emit('message', req.body.text);
-    console.log(req.body);
-    res.sendStatus(200);
+httpServer.listen(3000, () => {
+	console.log('listening on http://localhost:3000');
 });
-
-app.post('/parse/400', (req, res) => {
-    io.emit('message', '400' + req.body);
-    console.log('400');
-    res.sendStatus(400);
-});
-
-app.post('/parse/500', (req, res) => {
-    io.emit('message', '500');
-    console.log('500');
-    res.sendStatus(500);
-});
-
-io.on('connection', socket => {
-    console.log('connection');
-    socket.on('disconnect', () => {
-        console.log('disconnect');
-    });
-});
-
-httpServer.listen(3000);
